57ea6c45f8b37ea67d60c1600257bd460a9d6d9b
rafat <rafat@gmail.com>
2025-11-27 21:33:59 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
 <?php
-define('ZOHO_EMAIL', '967-712159295.380@zohomail.com');
-define('ZOHO_PASS',  'njVR1k5pa6qk'); // إذا كان هذا فعلاً App Password من Zoho
-?>
+define('BREVO_EMAIL', 'rafatkang@gmail.com'); // الإيميل الموثق في Brevo
+define('BREVO_API_KEY', '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');
