796fd1300265d6ba505cfb23e9bb720154b535ae
rafat <rafat@gmail.com>
2025-12-13 22:32:17 +0300

    updet

--- test_fcm.php
+++ test_fcm.php
@@ -4,8 +4,8 @@ require 'fcm_v1_manual.php';
 $testToken = 'd4B05-9oQVSAvz_GnRVtYy:APA91bEhYQZ63B85liQcEjDrX_1CJ1smi38BONdFnROJmjByW25pnOg00troDDPyOx4qZOcTvScr3jYC44mmaTOxj2TuehOFWR5HuxR8wqq27skorANZKIM';
 
 try {
-    $title = 'اختبار HTTP v1';
-    $body  = 'هذه رسالة تجريبية من PHP';
+    $title = 'Test HTTP v1';
+    $body  = 'This is a test message from PHP';
 
     $res = sendFcmV1ToTokenManual(
         $testToken,
